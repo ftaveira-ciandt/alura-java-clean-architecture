819ba378a7e8af45ef944910863723a67b7c6d55
Francisco Taveira <ftaveira@ciandt.com>
2023-03-08 17:48:30 -0300

    created aluno and email class

---
+++ src/main/java/org/escola/Aluno.java
@@ -0,0 +1,11 @@
+package org.escola;
+
+public class Aluno {
+
+    private String cpf;
+    private String nome;
+    private Email email;
+
+
+
+}
---
+++ src/main/java/org/escola/Email.java
@@ -0,0 +1,14 @@
+package org.escola;
+
+public class Email {
+
+    private String endereco;
+    private String destinatario;
+    private String assunto;
+    private String corpo;
+    private String remetente;
+    private String senha;
+
+
+
+}
