0e61a84b6cd80d38c6924dc4ae683c25b11914a3
Francisco Taveira <ftaveira@ciandt.com>
2023-03-13 16:30:37 -0300

    Created RepositorioDeALunosComJDBC class

--- src/main/java/br/com/alura/escola/dominio/aluno/Aluno.java
+++ src/main/java/br/com/alura/escola/dominio/aluno/Aluno.java
@@ -23,17 +23,18 @@ public class Aluno {
         this.telefones.add(new Telefone(ddd, numero));
     }
 
-    public CPF getCpf() {
-        return cpf;
+    public String getCpf() {
+        return cpf.getNumero();
     }
 
     public String getNome() {
         return nome;
     }
 
-    public Email getEmail() {
-        return email;
+    public String getEmail() {
+        return email.getEndereco();
     }
 
+    public List<Telefone> getTelefones() { return telefones; }
 
 }
